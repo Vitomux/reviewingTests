c124bee898360865cd0bd388b9841f234cec4993
Vitomux <vito.morici94@gmail.com>
2026-01-26 23:45:27 -0300

    Refactor scroll animation and cloning logic
    Updated the scroll animation logic and improved the cloning mechanism for better performance and visual effects.

--- scrollScaleTo/app.js
+++ scrollScaleTo/app.js
@@ -1,9 +1,6 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(ScrollTrigger, Flip);
 
-    //                Setear Lenis scroll 
- // -----------------------------------------------//
-    
     const lenis = new Lenis( );
     lenis.on('scroll', ScrollTrigger.update);
     gsap.ticker.add( ( time) => {
@@ -12,25 +9,22 @@ document.addEventListener("DOMContentLoaded", (event) => {
     gsap.ticker.lagSmoothing( 0 );
 
 
-        //            Elementos
- // -----------------------------------------------//
-
     let flipAnimation = null;
     const originalBox = document.querySelector(".box"); // caja objetivo
     const clonWrapper = document.querySelector(".clonWrapper"); // contenedor de nuestro clon
     const space = document.querySelector(".space") // div padre
 
 
-    //                Configuración
- // -----------------------------------------------//
 
     if ( originalBox && clonWrapper ) {
         const clonnedBox = originalBox.cloneNode(true); // clonamos nuestro objetivo ().box)
         clonWrapper.appendChild( clonnedBox ); // lo ubicamos dentro del wrapper
+
         gsap.set( originalBox, { opacity: 0 } ); // desvanecemos nuestro objetivo
         const rectSpace = space.getBoundingClientRect( ); // obtenemos coordenadas y espacialidad de nuestro div padre
+        const rectClon = clonWrapper.getBoundingClientRect( );
+        const rectTarget = originalBox.getBoundingClientRect( );
 
-        // ----Configuramos desde GSAP el estado del objeto a animar ( wrapper que contiene el clon ) ----//
 
         gsap.set( clonWrapper, { 
             position: "fixed",
@@ -42,48 +36,43 @@ document.addEventListener("DOMContentLoaded", (event) => {
         });
 
 
-        //----- Configuramos nuestra animación con Flip y el estado de esta ---------- //
-
         const state = Flip.getState( clonWrapper ); // el estado actual de nuestro clon
-
-        // seteamos el estado final
-        
         gsap.set( clonWrapper, {   
-            width: "100%",
-            height: rectSpace.height, // Le damos las direcciones de expansion a nuestro clon 
-            left: rectSpace.left,        // desde las coordenadas de rectSpace ( coordenadas de nuestro padre )
-            right: rectSpace.right, // asi aseguramos expansion limpia y matematicamente correcta hasta alcanzar objetivo
-            top: rectSpace.top,
-            });
+                width: "100%",
+                height: rectSpace.height, // Le damos las direcciones de expansion a nuestro clon 
+                left: rectTarget.left,        // Que adopte la altura de space, que se expanda hacia los lados del target y sea empujado desde el top del clon
+                right: rectTarget.right, // asi aseguramos expansion limpia y matematicamente correcta hasta alcanzar objetivo
+                top: rectClon.top,
+        });
 
-        flipAnimation = Flip.from( state, {  // y las propiedades de nuestra animación Como vamos del principio al final
-            duration: 1,
-            ease: "none",
-            paused: true,
+        flipAnimation = Flip.from( state, {
+                duration: 2,
+                ease: "power3.in",
+                paused: true,
         });
 
         ScrollTrigger.create( {
-            trigger: ".main",
-            start: "top top",
-            end: ( ) => `+=${ window.innerHeight * 1 }`, // termina la animacion una vez alcanzada la altura de la pantalla
-            pin: true,
-            scrub: true,
-            onUpdate: ( self ) => {  // a medida que se actualiza el scroll, sucede nuestra animación
-                flipAnimation.progress( self.progress ); 
-                if ( self.progress >= 1) {                         
-                    gsap.set( clonnedBox, { opacity: 0 } );   // seteamos la opacidad de nuestro clon, 
-                    gsap.set( originalBox, { opacity: 1 } );   // al encontrarse con nuestro objetivo cambian de opacidades
-                } else {
+                trigger: space,
+                start: "center",
+                end: ( ) => `+=${  ( rectSpace.height * 1) - rectClon.bottom }`, // la altura de space (div padre) menos el espacio entre el fondo de nuestro clon (div hijo) y el top del viewport nos dan como resultado la distancia entre el clon y la proxima seccion (nuestro objetivo)
+                pin: true,
+                scrub: true,
+                markers: true,
+                onUpdate: ( self ) => {  // a medida que se actualiza el scroll, sucede nuestra animación
+                    flipAnimation.progress( self.progress ); 
+                    if ( self.progress >= 1) {                         
+                        gsap.set( clonnedBox, { opacity: 0 } );   // seteamos la opacidad de nuestro clon, 
+                        gsap.set( originalBox, { opacity: 1 } );   // al encontrarse con nuestro objetivo cambian de opacidades
+                    } else {
+                        gsap.set( clonnedBox, { opacity: 1 } );
+                        gsap.set( originalBox, { opacity: 0 } );
+                    }
+                },
+                onLeaveBack: ( ) => { // ------------- En caso de subir el scroll, vuelven a las opacidades originales 
+                    flipAnimation.progress( 0 );
                     gsap.set( clonnedBox, { opacity: 1 } );
                     gsap.set( originalBox, { opacity: 0 } );
                 }
-},
-
-onLeaveBack: () => { // ------------- En caso de subir el scroll, vuelven a las opacidades originales 
-    flipAnimation.progress( 0 );
-    gsap.set( clonnedBox, { opacity: 1 } );
-    gsap.set( originalBox, { opacity: 0 } );
-}
         });
     }
 }); 
